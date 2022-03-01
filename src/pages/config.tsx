490223032dc764dd03b6bc6ad9a50da6cd10d39b
Christopher Angelo <angelo@angeloanan.xyz>
2022-03-01 23:53:07 +0700

    feat: add config page

---
+++ src/pages/config.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import * as React from 'react'
+
+const ConfigPage: NextPage = () => {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center'>
+      <h1 className='text-5xl font-bold'>Config Page</h1>
+      <main className='pt-8 text-2xl font-light'>
+        Sike, no config page is needed. You are good to go!
+      </main>
+    </div>
+  )
+}
+
+export default ConfigPage
