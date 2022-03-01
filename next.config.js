f802ecc1b0ef5a347d8bf2864393301a7b68c53d
Christopher Angelo <angelo@angeloanan.xyz>
2022-03-02 00:06:30 +0700

    feat: redirect main to panel

--- next.config.js
+++ next.config.js
@@ -12,7 +12,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  optimizeFonts: true
+  optimizeFonts: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/panel'
+      }
+    ]
+  }
 }
 
 module.exports = withPlugins([[withBundleAnalyzer]], nextConfig)
