845843112304e81b8f6b335e9c0ada3473979037
Christopher Angelo <angelo@angeloanan.xyz>
2022-03-01 20:03:02 +0700

    fix: use client_credentials to auth

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -4,7 +4,7 @@ const Auth: NextApiHandler = async (req, res) => {
   res.redirect(
     `https://id.twitch.tv/oauth2/authorize?client_id=sohzczuav4izhywl7yprw5lxubtfqn&redirect_uri=${encodeURIComponent(
       'https://bonkmarmu-leaderboard.vercel.app/api/callback'
-    )}&response_type=token&scope=channel:read:redemptions`
+    )}&response_type=client_credentials&scope=channel:read:redemptions`
   )
 }
 
